f365f9088f9e46bfcf2613a2645474ed06b55547
IlyaMatsuev <matsuev2000@mail.ru>
2019-02-25 22:14:29 +0300

    First and second labs

---
+++ Lab2/src/by/belstu/it/Matsuev/basejava/Anonymous.java
@@ -0,0 +1,8 @@
+package by.belstu.it.Matsuev.basejava;
+
+public class Anonymous {
+
+    void Replace(char oldchar, char newchar){ }
+
+    void Delete(char newchar){ }
+}
---
+++ Lab2/src/by/belstu/it/Matsuev/basejava/WrapperString.java
@@ -0,0 +1,45 @@
+package by.belstu.it.Matsuev.basejava;
+
+import java.util.Objects;
+/** Wrapper class for strings
+ *  @author Asus*/
+public class WrapperString {
+    private String someStr;
+
+    public WrapperString(String someStr) {
+        this.someStr = someStr;
+    }
+    /** Setter to initialize field {@link WrapperString#someStr}
+     *  @see WrapperString*/
+    public void setSomeStr(String someStr) {
+        this.someStr = someStr;
+    }
+
+    public String getSomeStr() {
+        return someStr;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        WrapperString that = (WrapperString) o;
+        return Objects.equals(someStr, that.someStr);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(someStr);
+    }
+
+    @Override
+    public String toString() {
+        return "WrapperString{ someStr=" + someStr + "}";
+    }
+
+    public void Replace(char oldchar, char newchar){
+        System.out.println("hello world");
+    }
+}
